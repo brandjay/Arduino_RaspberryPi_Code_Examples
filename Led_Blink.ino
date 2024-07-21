55a6d1f75c75fedde27b3edb176aeb8736a1aa8c
Brandon <brandon101morgan@outlook.com>
2024-07-21 18:55:16 -0500

    Code used to make the number 7

--- Led_Blink.ino
+++ Led_Blink.ino
@@ -1,14 +1,30 @@
+
+const int segmentA = 2;
+const int segmentB = 3;
+const int segmentC = 4;
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  //pinMode(13, OUTPUT);
   //int a = 13;
-  
+  pinMode(segmentA, OUTPUT);
+  pinMode(segmentB, OUTPUT);
+  pinMode(segmentC, OUTPUT);
+
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+
+
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  //delay(2000);
-  //digitalWrite(13, LOW);
-  //delay(1000);
+  /*  delay(2000);
+    digitalWrite(13, LOW);
+    delay(1000);
    if (5 < 6 ){
     digitalWrite( 13, HIGH);
     delay(800);
@@ -17,6 +33,7 @@ void loop() {
   }
   else {
     digitalWrite(13, LOW);
+    */
     
-    }
-}
+    
+ }
