726a84d03f85abdaed94e2c1d58e4a3a7ced11a7
Brandon <brandon101morgan@outlook.com>
2024-07-22 20:23:00 -0500

    Update and rename Led_Blink.ino to 7segementLED.ino

---
+++ 7segementLED.ino
@@ -0,0 +1,21 @@
+//This displays the number 7 using a 7 segment LED 
+const int segmentA = 2;
+const int segmentB = 3;
+const int segmentC = 4;
+
+void setup() {
+  
+  pinMode(segmentA, OUTPUT);
+  pinMode(segmentB, OUTPUT);
+  pinMode(segmentC, OUTPUT);
+
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+
+
+}
+void loop() {
+    
+    
+ }
