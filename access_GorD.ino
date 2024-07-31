cf328a4184f4036c302c11232a4a5ceabff13810
Brandon <brandon101morgan@outlook.com>
2024-07-31 17:21:03 -0500

    Add files via upload

--- access_GorD.ino
+++ access_GorD.ino
@@ -9,7 +9,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 int lightPin = 3;
 int denyPin = 5;
 
-void setup() {
+void setup() { 
   // initialize digital pin LED_BUILTIN as an output.
 
   Serial.begin(9600);                                           // Initialize serial communications with the PC
@@ -17,6 +17,7 @@ void setup() {
   mfrc522.PCD_Init();                                              // Init MFRC522 card
   Serial.println(F("Read personal data on a MIFARE PICC:"));  //shows in serial that it is ready to read
   pinMode(lightPin, OUTPUT);
+  pinMode(denyPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
